a4a7ab89014cf5740a51299e9441cf124cedfa71
erdemakkuzu <m.erdemakkuzu@gmail.com>
2021-04-10 10:46:04 +0300

    Initial Spring Boot Project

---
+++ src/main/java/com/testinc/centralizedpaymentsystem/CentralizedPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.testinc.centralizedpaymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedPaymentSystemApplication.class, args);
+	}
+
+}
